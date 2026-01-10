c4b416b0288a05402d4036360b05b10783d82ea2
junsu_Kim <96895747+junsu22@users.noreply.github.com>
2026-01-10 21:45:30 +0900

    Add files via upload

---
+++ practice/0110/treeHit.py
@@ -0,0 +1,18 @@
+treeHit = 0
+while treeHit <10:
+        treeHit += +1  # = treeHit
+        print("나무를 %d 번 찍었습니다!"% treeHit)
+        if treeHit == 10:
+            print("나무가 넘어갑니다.")
+
+
+
+
+Prompt = """
+1. Add
+2. Del
+3. List
+4. Quit
+Enter number: """
+
+
